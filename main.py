10de1d1b98af984b47ce6a2e48c7cd5d1fa18bf2
sachin62025 <sachinkumar18449@iiitmanipur.ac.in>
2026-01-03 15:33:43 +0530

    fixe issues

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ app.include_router(router)
 async def root():
     return {"message": "AgriGPT API is running. "}
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
